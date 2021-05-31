9dd7781c6810d7aedff3f38d1fea269d4ef65795
zedwar <1245918327@qq.com>
2021-05-31 14:15:51 +0800

    完成了each、find

---
+++ src/dom.js
@@ -0,0 +1,27 @@
+window.dom = {
+    style(node, name, value) {
+        if (arguments.length === 3) {
+            // dom.style(div, 'color', 'red')
+            node.style[name] = value
+        } else if (arguments.length === 2) {
+            if (typeof name === 'string') {
+                // dom.style(div, 'color')
+                return node.style[name]
+            } else if (name instanceof Object) {
+                // dom.style(div, {color: 'red'})
+                const object = name
+                for (let key in object) {
+                    node.style[key] = object[key]
+                }
+            }
+        }
+    },
+    find(selector, scope) {
+        return (scope || document).querySelectorAll(selector)
+    },
+    each(nodeList, fn) {
+        for (let i = 0; i < nodeList.length; i++) {
+            fn.call(null, nodeList[i])
+        }
+    }
+};
